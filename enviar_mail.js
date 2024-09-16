3251a1ba883cdbc7128ea370b1d6c8570c7082c3
braguilera <braaguileraa@gmail.com>
2024-09-16 11:26:18 -0300

    Feedback mejorado al enviar un form

--- enviar_mail.js
+++ enviar_mail.js
@@ -1,13 +1,17 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Variables para los campos del formulario
+    const submitButton = document.querySelector('.form__cta');
+    
+    submitButton.disabled = true;
+    animateDots(submitButton);
+    submitButton.style.backgroundColor = '#9050DD';
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
 
-    // Parámetros de EmailJS
     const templateParams = {
         name: name,
         email: email,
@@ -15,25 +19,44 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         message: message,
     };
 
-    // Enviar el email usando EmailJS
     emailjs.send("braaguileraa", "template_tpcebzi", templateParams)
         .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            showPopup();
-        })
-        .catch(function(error) {
-            console.error('FAILED...', error);
-            showPopup('Hubo un error al enviar el mensaje');
+            clearInterval(dotInterval);
+            submitButton.value = 'Mensaje enviado';
+            submitButton.style.transition = 'background-color 0.5s ease';
+            submitButton.style.backgroundColor = 'green';
+
+            document.getElementById('contactForm').reset();
+
+            setTimeout(() => {
+                submitButton.value = 'Enviar';
+                submitButton.disabled = false;
+                submitButton.style.backgroundColor = '#ae89db';
+            }, 3000);
+        },
+        function(error) {
+            clearInterval(dotInterval);
+            submitButton.value = 'Error al enviar';
+            submitButton.style.backgroundColor = 'red'; 
+
+            setTimeout(() => {
+                submitButton.value = 'Enviar';
+                submitButton.disabled = false;
+                submitButton.style.backgroundColor = '#ae89db';
+            }, 3000);
         });
 
 });
 
-function showPopup() {
-    const popup = document.getElementById('popup');
-    popup.classList.add('show');
-
-    // Ocultar el popup después de 3 segundos
-    setTimeout(function() {
-        popup.classList.remove('show');
-    }, 3000);
+let dotInterval;
+function animateDots(button) {
+    let dots = '';
+    dotInterval = setInterval(() => {
+        if (dots.length < 3) {
+            dots += '.';
+        } else {
+            dots = '';
+        }
+        button.value = 'Enviando mensaje' + dots;
+    }, 500);
 }
