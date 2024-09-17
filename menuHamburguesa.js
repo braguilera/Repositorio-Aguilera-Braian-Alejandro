d3819f0c1acc50d882f6fc058846ff8293d6c9ed
braguilera <braaguileraa@gmail.com>
2024-09-17 16:40:11 -0300

    arreglos en el header

--- menuHamburguesa.js
+++ menuHamburguesa.js
@@ -2,14 +2,7 @@
 const hamburgerIcon = document.getElementById('hamburger-icon');
 const menuContainer = document.getElementById('menu-container');
 
-// Agregar evento al ícono de hamburguesa para abrir/cerrar el menú
+// Evento al hacer clic en el ícono del menú hamburguesa
 hamburgerIcon.addEventListener('click', function() {
     menuContainer.classList.toggle('show-menu');
-});
-
-// Opción: cerrar el menú cuando se hace clic fuera de él
-document.addEventListener('click', function(event) {
-    if (!menuContainer.contains(event.target) && !hamburgerIcon.contains(event.target)) {
-        menuContainer.classList.remove('show-menu');
-    }
 });
