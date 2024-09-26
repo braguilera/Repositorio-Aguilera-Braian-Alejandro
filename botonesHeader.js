9d34101f8c23a18badd677b91a625afc46bc001e
braguilera <braaguileraa@gmail.com>
2024-09-26 11:24:17 -0300

    Funcionalidad de cambio de idioma finalizado

--- botonesHeader.js
+++ botonesHeader.js
@@ -1,6 +1,29 @@
+//Funcionalidad para modo claro y oscuro
+
 const btnTheme = document.querySelector('#theme-btn')
 
 btnTheme.addEventListener('click', () =>{
     document.body.classList.toggle('dark-mode');
     btnTheme.classList.toggle('active')
 });
+
+//Funcionalidad para cambiar idioma
+
+const langButtons = document.querySelectorAll("[data-language]");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+langButtons.forEach((button) => {
+    button.addEventListener("click", () =>{
+        fetch(`./${button.dataset.language}.json`)
+            .then(res => res.json())
+            .then(data => {
+                textsToChange.forEach((el) => {
+                    const section = el.dataset.section;
+                    const value = el.dataset.value;
+
+                    el.innerHTML = data[section][value];
+                })
+            })
+    })
+
+})
